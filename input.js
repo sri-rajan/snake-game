0c25e5844da7a68eceb9cdcf164ddae894b339fb
sri rajan <sriraja781@gmail.com>
2021-12-21 00:36:01 +0530

    edited for mobile mode

--- input.js
+++ input.js
@@ -1,6 +1,12 @@
 let inputDirection = { x: 0, y: 0};
 let lastInputDirection = {x: 0, y:0};
 
+//buttons
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
 window.addEventListener('keydown', e=>{
     switch(e.key){
         case 'ArrowUp':
@@ -20,6 +26,21 @@ window.addEventListener('keydown', e=>{
             inputDirection = {x: 1, y:0};
     }
 })
+
+up.onclick =function(){
+    if(lastInputDirection.y ==0) inputDirection = {x: 0,y: -1};
+}
+down.onclick =function(){
+    if(lastInputDirection.y ==0) inputDirection = {x: 0,y: 1};
+}
+left.onclick =function(){
+    if(lastInputDirection.x ==0) inputDirection = {x: -1,y: 0};
+}
+right.onclick =function(){
+    if(lastInputDirection.x ==0) inputDirection = {x: 1,y: 0};
+}
+
+
 export function getInputDirection(){
     lastInputDirection = inputDirection;
     return inputDirection;
